1c513545e0d203b0662921904a276a500c084f31
Joshi <Joshua.Peller@hotmail.de>
2023-10-15 17:12:39 +0200

    added THI-Board

---
+++ STMWorkspace/thi-board/Debug/Drivers/Display/Src/_mcpr_aufgabe3.d
@@ -0,0 +1,22 @@
+Drivers/Display/Src/_mcpr_aufgabe3.o: \
+ ../Drivers/Display/Src/_mcpr_aufgabe3.c \
+ ../Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f407xx.h \
+ ../Drivers/CMSIS/Include/core_cm4.h \
+ ../Drivers/CMSIS/Include/cmsis_version.h \
+ ../Drivers/CMSIS/Include/cmsis_compiler.h \
+ ../Drivers/CMSIS/Include/cmsis_gcc.h \
+ ../Drivers/CMSIS/Include/mpu_armv7.h \
+ ../Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
+ I:/05_Coding/ffi3_intp/thi-board/Drivers/Display/Inc/mcpr_timer.h \
+ I:/05_Coding/ffi3_intp/thi-board/Drivers/Display/Inc/_mcpr_aufgabe3.h \
+ I:/05_Coding/ffi3_intp/thi-board/Drivers/Display/Inc/display.h
+../Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f407xx.h:
+../Drivers/CMSIS/Include/core_cm4.h:
+../Drivers/CMSIS/Include/cmsis_version.h:
+../Drivers/CMSIS/Include/cmsis_compiler.h:
+../Drivers/CMSIS/Include/cmsis_gcc.h:
+../Drivers/CMSIS/Include/mpu_armv7.h:
+../Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
+I:/05_Coding/ffi3_intp/thi-board/Drivers/Display/Inc/mcpr_timer.h:
+I:/05_Coding/ffi3_intp/thi-board/Drivers/Display/Inc/_mcpr_aufgabe3.h:
+I:/05_Coding/ffi3_intp/thi-board/Drivers/Display/Inc/display.h:
---
+++ STMWorkspace/thi-board/Drivers/Display/Inc/_mcpr_aufgabe3.h
@@ -0,0 +1,30 @@
+#ifndef __MCPR_AUFGABE3_H
+#define __MCPR_AUFGABE3_H
+
+#include <inttypes.h>
+
+#define DISPLAY_WIDTH 320
+#define DISPLAY_HEIGHT 240
+#define MAX_STR_LEN 20
+
+#define BACKGROUND 0xFFFF
+#define BLACK 0x0000
+
+extern void mcpr_LCD_WriteData(uint16_t data);
+extern void mcpr_LCD_WriteCommand(uint16_t data);
+extern void mcpr_LCD_InitPorts(void);
+extern void mcpr_LCD_Init(void);
+extern void mcpr_LCD_WriteReg(uint16_t cmd, uint16_t data);
+
+extern void mcpr_LCD_Output16BitWord(uint16_t data);
+extern void mcpr_LCD_SetCursor(const uint16_t x, const uint16_t y);
+extern void mcpr_LCD_DrawPixel(uint16_t color);
+extern void mcpr_LCD_ClearDisplay(uint16_t color);
+
+extern void mcpr_LCD_WriteLetter(uint16_t x, uint16_t y, uint16_t colorfg, uint16_t colorbg, uint8_t c);
+extern void mcpr_LCD_WriteString(uint16_t x, uint16_t y, uint16_t colorfg, uint16_t colorbg, char str[MAX_STR_LEN]);
+
+__attribute__((noreturn)) extern void mcpr_DisplayMessung(void);
+extern void mcpr_DisplayMessungV2(void);
+extern void mcpr_DisplayMain(void);
+#endif
